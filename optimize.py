3b86b53b08439127e9e20b2b16e980c5fbc2fc7a
Zeyu Wang <wzyfrank@live.com>
2020-01-09 18:49:10 -0800

    update binary variables

--- optimize.py
+++ optimize.py
@@ -17,7 +17,7 @@ def OptimalBatteryDispatch(P, E, debug=False):
     P_chg = cp.Variable(H)
     P_dsg = cp.Variable(H)
     E_soc = cp.Variable(H+1)
-    # gamma = cp.Variable(H, boolean=True)
+    gamma = cp.Variable(H, boolean=True)
 
     # objective functions
     objective = cp.Minimize(pi_energy * (P_chg - P_dsg))
@@ -30,22 +30,33 @@ def OptimalBatteryDispatch(P, E, debug=False):
     constraints += [E_soc[0] == E_soc[-1]]
 
     for i in range(H):
-        # constraints += [0 <= gamma[i] * P_chg[i], gamma[i] * P_chg[i] <= P]
-        # constraints += [0 <= (1 - gamma[i]) * P_dsg[i], (1 - gamma[i]) * P_dsg[i] <= P]
+        constraints += [0 * gamma[i] <= P_chg[i]]
+        constraints += [P_chg[i] <= P * gamma[i]]
+        constraints += [0 * (1 - gamma[i]) <= P_dsg[i]]
+        constraints += [P_dsg[i] <= (1 - gamma[i]) * P]
         constraints += [E_soc[i+1] == E_soc[i] + eta_chg * P_chg[i] - P_dsg[i] / eta_dsg]
 
     # form the problem and solve.
     prob = cp.Problem(objective, constraints)
-    prob.solve()
+    print('start solving the problem')
+    prob.solve(verbose=False)
 
     result_power = np.round(P_chg.value - P_dsg.value, 2)
 
     result_energy = np.round(E_soc[:-1].value, 2)
     if debug:
+        print('\n')
+        print('the objective value is: %.2f' % prob.objective.value)
+        print('\n')
+        print('the charging/discharging decisions are: ')
+        print(gamma.value)
+        print('\n')
         print('the charging power results are: ')
         print(result_power)
+        print('\n')
         print('the energy stored are: ')
         print(result_energy)
+
 
 
 if __name__ == "__main__":
