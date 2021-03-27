fd6962b86e6295971cf3fa561d9a3fbfd0407d55
Zeyu Wang <wzyfrank@live.com>
2021-03-27 16:50:42 -0700

    Update lmp_reader.py
    Add util functions

--- lmp_reader.py
+++ lmp_reader.py
@@ -5,23 +5,54 @@ import os
 # how many hours one day
 H = 24
 
+FILE_NAME = "data/2011-rt.csv"
 
+# Read the LMP data, file I/O
+def read_data():
+    cwd = os.getcwd()
+    
+    # the .csv file with locational marginal prices.
+    data = pd.read_csv(os.path.join(cwd, FILE_NAME), header=None, index_col=False)
+    array = data.to_numpy()
+    array = array / 1000
+    return array
+
+# Given the param "seed" and "day", return the prices.    
+def get_lmp_with_data(data, seed, day):
+    prices = data[seed][day * H: day * H + H]
+    prices.reshape(1, H)
+    return prices
+    
 # get the real-time locational marginal price
 # time period： 24 hours
 # interval: hour
 # unit $/kWh
-def get_lmp(sample, day, debug=False):
+def get_lmp(seed, day, debug=False):
     cwd = os.getcwd()
 
     # the .csv file with locational marginal prices.
-    data = pd.read_csv(os.path.join(cwd, "data/files/2011-rt.csv"), header=None, index_col=False)
+    data = pd.read_csv(os.path.join(cwd, FILE_NAME), header=None, index_col=False)
 
     array = data.to_numpy()
-    prices = array[sample][day * H: day * H + H]
-    prices = prices / 1000
-
+    prices = array[seed][day * H: day * H + H]
+    
     if debug:
         print('get lmp the lmp prices are: ')
         print(prices)
 
     return prices
+
+def get_dimension(debug=False):
+    cwd = os.getcwd()
+    
+    # the .csv file with locational marginal prices.
+    data = pd.read_csv(os.path.join(cwd, FILE_NAME), header=None, index_col=False)
+    
+    return data.shape
+    
+if __name__ == "__main__":
+    debug = True
+    # pi_energy = get_lmp(5, 200, debug)
+    
+    dim = get_dimension(debug)
+    print(dim)
